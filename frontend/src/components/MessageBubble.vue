053b8306eda57073b67fc6a87e853b4fb5248be3
星难 <2386016253@qq.com>
2026-06-16 14:24:11 +0800

    fix: 修复流式输出时内容被隐藏的问题，改为实时显示文字+闪烁光标

--- frontend/src/components/MessageBubble.vue
+++ frontend/src/components/MessageBubble.vue
@@ -20,14 +20,8 @@
         </span>
       </div>
 
-      <div v-if="!isTyping" class="text-sm leading-relaxed whitespace-pre-wrap">
-        {{ content }}
-      </div>
-
-      <div v-else class="flex items-center gap-1">
-        <span class="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style="animation-delay: 0ms"></span>
-        <span class="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style="animation-delay: 150ms"></span>
-        <span class="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style="animation-delay: 300ms"></span>
+      <div class="text-sm leading-relaxed whitespace-pre-wrap">
+        {{ content }}<span v-if="isTyping" class="inline-block w-2 h-4 ml-0.5 bg-gray-400 align-middle animate-pulse"></span>
       </div>
 
       <div 
